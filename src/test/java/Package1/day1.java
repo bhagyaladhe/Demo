d0c6a852eb1dff5728fbcdc4f197e20b05e04c3a
bhagyaladhe <bhagyashriladhe2@gmail.com>
2025-10-28 18:48:08 +0530

    checking

---
+++ src/test/java/Package1/day1.java
@@ -0,0 +1,17 @@
+package Package1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class day1 {
+
+    public static void main(String[] args) {
+        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\Dell\\Desktop\\STAD\\Software\\Chrome\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        WebDriverManager.chromedriver().setup();  // Automatically downloads matching driver
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.instagram.com/");
+        driver.quit();
+
+    }
+}
