f168b2120958bb43c8b21de87c532f9fffb1cacd
bhagyaladhe <bhagyashriladhe2@gmail.com>
2025-10-30 15:00:55 +0530

    Changes

--- src/test/java/Package1/day1.java
+++ src/test/java/Package1/day1.java
@@ -10,7 +10,7 @@ public class day1 {
         //System.setProperty("webdriver.chrome.driver", "C:\\Users\\Dell\\Desktop\\STAD\\Software\\Chrome\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
         WebDriverManager.chromedriver().setup();  // Automatically downloads matching driver
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.googlegit.com/");
+        driver.get("https://www.googlr.com/");
         driver.quit();
         System.out.println("-------------Changes------------------------");
 
