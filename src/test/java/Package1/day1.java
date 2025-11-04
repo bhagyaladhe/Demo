f4a24c900d5d1c8c3369027c0dfa605a9ffecfdd
bhagyaladhe <bhagyashriladhe2@gmail.com>
2025-11-04 22:40:19 +0530

    m

--- src/test/java/Package1/day1.java
+++ src/test/java/Package1/day1.java
@@ -14,7 +14,7 @@ public class day1 {
         driver.get("https://www.googlr.com/");
         driver.quit();
 
-        System.out.println("-------------MOHIT tTTT-----------------------");
+        System.out.println("-------------MOHIT-----------------------");
 
 
 
